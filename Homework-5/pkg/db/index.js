cb6a7bd7236467d7d46bce11606519de9be3eaa9
Marija Zlatkova <marijazlatkova44@yahoo.com>
2023-09-12 13:37:44 +0200

    Domasna-5

---
+++ Homework-5/pkg/db/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const config = require("../config");
+
+const { MONGO_USERNAME, MONGO_PASSWORD } = config.getSection("development");
+
+const uri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.tonpgxf.mongodb.net/recipes?retryWrites=true&w=majority`;
+
+(async function () {
+  try {
+    await mongoose.connect(uri);
+    console.log("Successfully connected to DataBase");
+  } catch (err) {
+    console.log("Error connecting to DataBase", err);
+  }
+})();
